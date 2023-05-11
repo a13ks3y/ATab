573e9c2b750416327f605c2ea0c39b0bffc81f94
Oleksii Smirnov <o.smirnov@e-tender.ua>
2023-05-11 23:26:20 +0300

    remove unnecessary console.log and commented code.

--- src/js/getBookmarks.js
+++ src/js/getBookmarks.js
@@ -1,18 +1,13 @@
 function getBookmarks() {
     return new Promise(resolve => {
         chrome.bookmarks.getTree(tree => {
-            console.log(tree);
             let result = tree[0].children[0].children;
-            // subfolders.forEach(subfolder => {
-            //     result = [...result, ...subfolder.children.filter(ch => !ch.children)];
-            // });
-            //result = result.map(bm => ({...bm, title: removeImageFromTitle(bm.title)}));
             resolve(result);
         });
     });
 }
 function removeImageFromTitle(title) {
-    title = title.substr();
+    title = title.substr(0);
     title = title.replace(/Image [0-9]+\:/ig, '');    
     title = title.replace(/Post [0-9]+\:/ig, '');
     return title;
